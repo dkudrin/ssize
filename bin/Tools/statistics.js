391b9d42d4146d9fd275d6025f9b30585b46c086
dkudrin <dkudrin@gmail.com>
2016-04-18 14:26:12 +0000

    Change label from s to ssize

--- bin/Tools/statistics.js
+++ bin/Tools/statistics.js
@@ -43,4 +43,4 @@ var statistics = {
 };
 
 statistics.init();
-statistics.event({ action: 'Minimal size app opened', label: 's' , category: 'driverpack online'});
+statistics.event({ action: 'Minimal size opened', label: 'ssize' , category: 'driverpack online'});
